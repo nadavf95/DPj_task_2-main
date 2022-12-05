5a770938cf421c4e0a5b5f7732cc876384bf2cc8
Nadav Fuchs <nadavf95@gmail.com>
2022-12-05 15:41:55 +0200

    new files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //data info
-const API_BASE = 'https://www.balldontlie.io/api/v1/stats/?dates[]=';
+const API_BASE = 'https://www.balldontlie.io/api/v1/games/?dates[]=';
 
+let games_flag = false;
+let players_flag = false;
+
+let curr_game_info = null;
 
 // notice in our html we have a node with ID "app"
 // hint: use this reference later to inject data into your page
 const app = document.getElementById('app');
+
+
 
 // this function geting date parmeter adn feching data of 
 // all games at this date
@@ -22,45 +28,65 @@ async function queryEndpoint(date){
    }
   return data;
 }
+const submit =document.getElementById("submit-button");
 
-
-async function getGamesData(date){
-  // write you logic for getting the data from the API here
-  // return your data from this function
+submit.onclick = async function on_submit_click(){
+  // TO DO 
+  // get date from input box
+  let date = document.getElementById("date-input").value;
+  // fetch dada
   const gamesData = await queryEndpoint(date);
+  console.log(gamesData);
+  // crate templates
+  await renderGamesUI(gamesData);
 
-  
-  return data;
+  //show game section
+}
+
+function on_game_click(){
+  // get game info
+  // get players game info
+  // sort and filter
+  // show players section
 }
 
 function clearUI() {
-  clearPlayerUI()
-  // TO DO: clear games and than hide games sectom 
-}
-
-function clearPlayerUI(){
-  // TO DO: clear players and than hide player section
+  while (app.firstChild) {
+    app.removeChild(app.firstChild);
+  }
 }
 
 
 
 
-
-
-async function renderUI(data) {
-
+async function renderGamesUI(games_data) {
+  let games = Object.values(games_data.data);
+  let sort_val = document.getElementById("data-sort").value;
+  console.log(sort_val)
+  let filter = document.getElementById("data-filter").value;
+  console.log(filter)
+  console.log(games);
+  games = games.sort((a,b) => {
+    if (sort_val == "a-z"){
+      return a.home_team.name.localeCompare(b.home_team.name);
+    }
+    else{
+      return b.visitor_team_score + b.home_team_score - a.visitor_team_score - a.home_team_score;
+    }  
+  });
+  
   clearUI();
-
-  // you have your data! add logic here to render it to the UI
-  // notice in the HTML file we call render();
-  const dummyItemElement = Object.assign(document.createElement("div"), { className: "player" })
-  const dummyContentElement = Object.assign(document.createElement("div"), { className: "content" })
-  dummyContentElement.innerHTML = "player";
-  dummyItemElement.appendChild(dummyContentElement);
-  app.appendChild(dummyItemElement);
+  for (const game of games) {
+    if ((game.home_team.conference == filter)||(filter == "none")){
+      app.innerHTML += `
+      <button class="game-button" type="button" onclick="gamefunc()">
+        <div class="content">
+          <h2>${game.home_team.name} VS ${game.visitor_team.name}</h2>
+          <h3>${game.home_team_score} : ${game.visitor_team_score}</h3>
+          <p>${game.status}</p>
+        </div>
+      </button>`
+    }
+  }
 }
 
-// maybe to this:
-
-//const data = await getData();
-//await renderUI(data);
